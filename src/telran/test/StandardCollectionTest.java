443503d6677e1a4a35647ee51c1d39292560583f
Ovadya33 <timurgts2710@gmail.com>
2023-02-21 15:06:47 +0300

    ClassWork #33 chapter 1

---
+++ src/telran/test/StandardCollectionTest.java
@@ -0,0 +1,19 @@
+package telran.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StandardCollectionTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
