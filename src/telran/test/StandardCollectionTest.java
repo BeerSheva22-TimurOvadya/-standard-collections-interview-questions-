daa12ed93dd06b44824ce8f9c04f74ea4c772aef
Ovadya33 <timurgts2710@gmail.com>
2023-02-21 15:21:17 +0300

    ClassWork #33 Chapter1

--- src/telran/test/StandardCollectionTest.java
+++ src/telran/test/StandardCollectionTest.java
@@ -16,6 +16,10 @@ class StandardCollectionTest {
 		List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 70, -20));
 		list.add(5);
 		List<Integer> listSub = list.subList(6, 9);
+		
+		System.out.println(listSub);
+		listSub.add(1, -2);
+		System.out.println(list);
 	}
 
 }
