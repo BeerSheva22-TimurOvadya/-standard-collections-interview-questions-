16291f13ec3b75ac9157d39daa87fb8c39ec4f62
Ovadya33 <timurgts2710@gmail.com>
2023-02-22 19:39:36 +0300

    HomeWork #33

--- src/telran/test/StandardCollectionTest.java
+++ src/telran/test/StandardCollectionTest.java
@@ -1,9 +1,15 @@
 package telran.test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import telran.util.StackInt;
+
 import java.util.*;
-
-import org.junit.jupiter.api.*;
-
+import java.util.stream.Collectors;
 
 class StandardCollectionTest {
 
@@ -12,14 +18,67 @@ class StandardCollectionTest {
 	}
 
 	@Test
-	void test() {
+	@Disabled
+	void SubListtest() {
 		List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 70, -20));
 		list.add(5);
 		List<Integer> listSub = list.subList(6, 9);
-		
+
 		System.out.println(listSub);
 		listSub.add(1, -2);
+		listSub.sort(Integer::compare);
+		listSub.clear();
 		System.out.println(list);
+
+	}
+
+	@Test
+	@Disabled
+	void displayOccurrencesCount() {
+		String[] strings = { "lmn", "abc", "abc", "lmn", "a", "lmn" };
+		Arrays.stream(strings).collect(Collectors.groupingBy(s -> s, Collectors.counting())).entrySet().stream()
+				.sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
+				.forEach(e -> System.out.printf("%s: %d\n", e.getKey(), e.getValue()));
+
+	}
+
+	@Test
+//	@Disabled
+	void displayDigitStatistics() {
+		new Random().ints(1000000, 1, Integer.MAX_VALUE).mapToObj(num -> Integer.toString(num))
+				.flatMapToInt(String::chars).boxed().collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+				.entrySet().stream().sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
+				.forEach(e -> System.out.printf("Element: %1c, occurs: %d times.\n", e.getKey(), e.getValue()));
+	}
+
+	@Test
+	void stackIntTest() {
+		StackInt stack = new StackInt();
+		assertTrue(stack.isEmpty());
+		assertThrows(NoSuchElementException.class, stack::pop);
+		assertThrows(NoSuchElementException.class, stack::getMax);
+		stack.push(-20);
+		stack.push(-8);
+		stack.push(-134);
+		stack.push(58);
+		stack.push(0);
+		stack.push(6656);
+		stack.push(3);
+		assertFalse(stack.isEmpty());
+		assertEquals(3, stack.pop());
+		assertEquals(6656, stack.getMax());
+		assertEquals(6656, stack.pop());
+		assertEquals(58, stack.getMax());
+		stack.pop();
+		stack.pop();
+		assertEquals(-8, stack.getMax());
+		stack.pop();
+		stack.pop();
+		assertEquals(-20, stack.getMax());
+		stack.pop();		
+		assertTrue(stack.isEmpty());
+		assertThrows(NoSuchElementException.class, stack::getMax);
+		assertThrows(NoSuchElementException.class, stack::pop);
 	}
 
 }
---
+++ src/telran/util/StackInt.java
@@ -0,0 +1,46 @@
+package telran.util;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class StackInt {
+	private static LinkedList<Integer> stack = new LinkedList<>();
+	private static LinkedList<Integer> maxNumber = new LinkedList<>();
+
+	public void push(int num) {
+
+		stack.add(num);
+		if (maxNumber.isEmpty() || num >= maxNumber.getLast()) {
+			maxNumber.add(num);
+		}
+	}
+
+	public int pop() {
+		possibleException();
+		int res = stack.removeLast();
+		if (res == maxNumber.getLast()) {
+			maxNumber.removeLast();
+		}
+		return res;
+	}
+
+	public boolean isEmpty() {
+		return stack.isEmpty();
+	}
+
+	public int getMax() {
+		possibleException();
+		return maxNumber.getLast();
+	}
+
+	private void possibleException() {
+		if (stack.isEmpty()) {
+			throw new NoSuchElementException();
+		}
+	}
+}
